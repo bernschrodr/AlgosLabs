8e913520b4ea6d7b292ee700c41a3478bec6259f
bernschrodr <vlad60870@gmail.com>
2018-09-30 13:14:46 +0300

    merge sort

--- Section_2/Task_A/Task_A.c
+++ Section_2/Task_A/Task_A.c
@@ -1,80 +1,61 @@
 #include <stdio.h>
-#include <string.h>
-//Вариант 15
+#include <stdlib.h>
 
-/**
-  * Сортирует массив, используя рекурсивную сортировку слиянием
-  * up - указатель на массив, который нужно сортировать
-  * down - указатель на массив с, как минимум, таким же размером как у 'up', используется как буфер
-  * left - левая граница массива, передайте 0, чтобы сортировать массив с начала
-  * right - правая граница массива, передайте длину массива - 1, чтобы сортировать массив до последнего элемента
-  * возвращает: указатель на отсортированный массив. Из-за особенностей работы данной реализации
-  * отсортированная версия массива может оказаться либо в 'up', либо в 'down'
-  **/
-int* merge_sort(int *up, int *down, unsigned int left, unsigned int right)
+
+// Функция сортировки нисходящим слиянием
+void mergeSort(long long *a, long l, long r)
 {
-    if (left == right)
+  if (l == r) return; // границы сомкнулись
+  int mid = (l + r) / 2; // определяем середину последовательности
+  // и рекурсивно вызываем функцию сортировки для каждой половины
+  mergeSort(a, l, mid);
+  mergeSort(a, mid + 1, r);
+  int i = l;  // начало первого пути
+  int j = mid + 1; // начало второго пути
+  long long *tmp = (long long*)malloc(r * sizeof(long long)); // дополнительный массив
+  for (int step = 0; step < r - l + 1; step++) // для всех элементов дополнительного массива
+  {
+    // записываем в формируемую последовательность меньший из элементов двух путей
+    // или остаток первого пути если j > r
+    if ((j > r) || ((i <= mid) && (a[i] < a[j]))) 
     {
-        down[left] = up[left];
-        return down;
+      tmp[step] = a[i];
+      i++;
     }
-
-    unsigned int middle = (left + right) / 2;
-
-    // разделяй и властвуй
-    int *l_buff = merge_sort(up, down, left, middle);
-    int *r_buff = merge_sort(up, down, middle + 1, right);
-
-    // слияние двух отсортированных половин
-    int *target = l_buff == up ? down : up;
-
-    unsigned int l_cur = left, r_cur = middle + 1;
-    for (unsigned int i = left; i <= right; i++)
+    else 
     {
-        if (l_cur <= middle && r_cur <= right)
-        {
-            if (l_buff[l_cur] < r_buff[r_cur])
-            {
-                target[i] = l_buff[l_cur];
-                l_cur++;
-            }
-            else
-            {
-                target[i] = r_buff[r_cur];
-                r_cur++;
-            }
-        }
-        else if (l_cur <= middle)
-        {
-            target[i] = l_buff[l_cur];
-            l_cur++;
-        }
-        else
-        {
-            target[i] = r_buff[r_cur];
-            r_cur++;
-        }
+      tmp[step] = a[j];
+      j++;
     }
-    return target;
+  }
+  // переписываем сформированную последовательность в исходный массив
+  for (int step = 0; step < r - l + 1; step++)
+    a[l + step] = tmp[step];
 }
 
-int main() {
-
-    long long n;
+int main()
+{
 
     FILE *fin, *fout;
     fin = fopen("sort.in", "r");
     fout = fopen("sort.out", "w");
-    fscanf(fin, "%d", &n);
-    long long arr[n],arr2[n];
+    long n;
+    fscanf(fin, "%ld", &n);
+    long long arr[n], arr2[n];
 
     for (int i = 0; i < n; i++)
     {
-        fscanf(fin, "%f", &arr[i]);
+        fscanf(fin, "%lld", &arr[i]);
     }
 
-    memcpy(arr2, merge_sort(arr,arr2,0,1), n * sizeof(long long));
+    mergeSort(arr,1,n);
 
+    for (int i = 0; i < n; i++)
+    {
+        fprintf(fout, "%lld ", arr[i]);
+    }
 
+    fclose(fin);
+    fclose(fout);
     return 0;
 }
