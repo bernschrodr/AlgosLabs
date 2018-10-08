34c388bae5701f6e19d496e9795827df3b96a266
Vladislav <vlad60870@gmail.com>
2018-10-08 06:31:53 -0700

    Create Task_C.cpp

---
+++ Section_2/Task_C/Task_C.cpp
@@ -0,0 +1,76 @@
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Слияние
+void Merge(long a[], long left, long mid, long right, long Counter)
+{
+	long it1 = 0;
+	long it2 = 0;
+	long result[right - left];
+
+	while ((left + it1 < mid) & (mid + it2 < right)) {
+		if (a[left + it1] < a[mid + it2])
+		{
+			result[it1 + it2] = a[left + it1];
+			it1 += 1;
+		}
+		else
+		{
+			result[it1 + it2] = a[mid + it2];
+			it2 += 1;
+			Counter += (a.Length - it1);
+		}
+	}
+
+	while (left + it1 < mid)
+	{
+		result[it1 + it2] = a[left + it1];
+		it1 += 1;
+	}
+
+	while (mid + it2 < right)
+	{
+		result[it1 + it2] = a[mid + it2];
+		it2 += 1;
+	}
+
+	for (long i = 0; i < it1 + it2; i++)
+		a[left + i] = result[i];
+}
+
+//Рекурсивная сортировка слиянием
+void MergeSort(long long a[], long left, long right, long Counter)
+{
+	if (left + 1 >= right)
+		return;
+	long mid = (left + right) / 2;
+	MergeSort(a, left, mid, Counter);
+	MergeSort(a, mid, right, Counter);
+	Merge(a, left, mid, right, Counter);
+}
+int main()
+{
+	fstream fs;
+
+	fs.open("inversions.in", fstream::in);
+	
+	long size,k;
+	fs >> size;
+	long Inp[n];
+	
+	for (int i = 0; i < size; ++i) fs >> Inp[i];
+
+	fs.close();
+
+	MergeSort(Inp, 0, Inp.size() - 1, k);
+
+	fs.open("inversions.out", fstream::out);
+	fs << k;
+	fs.close();
+
+
+
+	return 0;
+}
