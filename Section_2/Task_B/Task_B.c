aae67332a41b242ac188a9f5f416e3f171b7207c
bernschrodr <vlad60870@gmail.com>
2018-10-08 20:26:30 +0300

    Task_B

--- Section_2/Task_B/Task_B.c
+++ Section_2/Task_B/Task_B.c
@@ -1,97 +1,117 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-
-// Слияние
-void Merge(char a[][10], long left, long mid, long right)
+void Merge(char Country[][10], char Name[][10], long Left, long Mid, long Right)
 {
-  long it1 = 0;
-  long it2 = 0;
-  char result[right - left][10];
+	long i, j, k;
+	long n1 = Mid - Left + 1;
+	long n2 = Right - Mid;
 
-  while ((left + it1 < mid) & (mid + it2 < right)){
-    if (strcmp (a[left + it1], a[mid + it2]) < 0)
-    {
-      strcpy(result[it1 + it2],a[left + it1]);
-      it1 += 1; 
-    }
-    else
-    {
-      strcpy(result[it1 + it2],a[mid + it2]);
-      it2 += 1;
-    }}
-    
-  while (left + it1 < mid)
-  {
-    strcpy(result[it1 + it2], a[left + it1]);
-    it1 += 1;
-  }
+	char Lcountries[Mid][10];
+	char Lsurnames[Mid][10];
+	char Rcountries[Mid][10]; 
+	char Rsurnames[Mid][10]; 
 
-  while (mid + it2 < right)
-  {
-    strcpy(result[it1 + it2], a[mid + it2]);
-    it2 += 1;
-  }
+	for (i = 0; i < n1; i++)
+	{
+		
+		strcpy(Lcountries[i], Country[Left + i] );
+		strcpy(Lsurnames[i], Name[Left + i]);
+	}
+	for (j = 0; j < n2; j++)
+	{
+		
+		strcpy(Rcountries[j], Country[Mid + 1 + j] );
+		strcpy(Rsurnames[j], Name[Mid + 1 + j] );
+	}
 
-  for (long i = 0; i < it1 + it2; i++)
-    strcpy(a[left + i], result[i]);
+	i = 0;
+	j = 0;
+	k = Left;
+
+	while (i < n1 && j < n2)
+	{
+		if (Lcountries[i] <= Rcountries[j])
+		{
+      strcpy(Country[k], Lcountries[i]);
+      strcpy(Name[k], Lsurnames[i]);
+			k++;
+			i++;
+		}
+		else
+		{
+       strcpy(Country[k], Rcountries[j]);
+      strcpy(Name[k], Rsurnames[j]);       
+			k++;
+			j++;
+		}
+	}
+
+	while (i < n1)
+	{
+    strcpy(Country[k], Lcountries[i]); 
+    strcpy(Name[k],Lsurnames[i]);
+		k++;
+		i++;
+	}
+	while (j < n2)
+	{
+    strcpy(Country[k], Rcountries[j]); 
+		strcpy(Name[k], Rsurnames[j]);
+		k++;
+		j++;
+	}
 }
 
-//Рекурсивная сортировка слиянием
-void MergeSort(char a[][10], long left, long right)
+void MergeSort(char Country[][10], char Name[][10], long Left, long Right)
 {
-  if (left + 1 >= right)
-    return;
-  long mid = (left + right) / 2;
-  MergeSort(a, left, mid);
-  MergeSort(a, mid, right);
-  Merge(a, left, mid, right);
-}
+	if (Right > Left)
+	{
+		int Mid = (Right + Left) / 2;
+		MergeSort(Country, Name, Left, Mid);
+		MergeSort(Country, Name, Mid + 1, Right);
 
+		Merge(Country, Name, Left, Mid, Right);
+	}
+}
 
 void main()
 {
 
-    FILE *fin, *fout;
-    fin = fopen("race.in", "r");
-    fout = fopen("race.out", "w");
-    long n;
-    long k = 0;
-    fscanf(fin, "%ld", &n);
-    char Country[n][10], Name[n][10], SortedCountries[n][10], SortedCountriesOne[200][10];
-    
-    //Ввод
-    for (int i = 0; i < n; i++)
+  FILE *fin, *fout;
+  fin = fopen("race.in", "r");
+  fout = fopen("race.out", "w");
+  long n;
+  fscanf(fin, "%ld", &n);
+  char Country[n][10], Name[n][10];
+
+  //Ввод
+  for (long i = 0; i < n; i++)
+  {
+    fscanf(fin, "%s", Country[i]);
+    fscanf(fin, "%s", Name[i]);
+  }
+
+  //Сортировка по алфавиту
+  MergeSort(Country, Name, 0, n-1); 
+
+  //вывод
+  for (long i = 0; i < n; i++)
+  {
+    if (i == 0)
     {
-        fscanf(fin, "%s", Country[i]);
-        fscanf(fin, "%s", Name[i]);
+      fprintf(fout, "=== %s ===\n", Country[i]);
+      fprintf(fout, "%s\n", Name[i]);
     }
-
-    //Копируем массив, который будем сортировать
-    memcpy (SortedCountries, Country, sizeof(Country)); 
-    
-    //Копируем первый элемент для сравнения 
-    strcpy(SortedCountriesOne[0], Country[0]);
-
-    //Сортировка по алфавиту
-    MergeSort(SortedCountries, 0, n);
-
-    //Создаем массив без повторяющихся элементов
-    for (int i = 1; i <= n; i++)
-        if (strcmp(SortedCountries[i], SortedCountries[i - 1]) != 0)
-        {
-            ++k;
-            strcpy(SortedCountriesOne[k], SortedCountries[i]);
-        }
-
-    //вывод
-    for(long i = 0; i < k; i++){
-        fprintf(fout, "=== %s ===\n", SortedCountriesOne[i]);
-        for(long j = 0; j < n; j++){
-            if(strcmp(SortedCountriesOne[i], Country[j]) == 0){
-                fprintf(fout, "%s\n", Name[j]);
-            }
-        }
-
+    else if (strcmp(Country[i], Country[i - 1]) == 0)
+    {
+      fprintf(fout, "%s\n", Name[i]);
     }
+    else
+    {
+      fprintf(fout, "=== %s ===\n", Country[i]);
+      fprintf(fout, "%s\n", Name[i]);
+    }
+  }
 }
--- Section_2/Task_B/Task_B2.c
+++ Section_2/Task_B/Task_B2.c
@@ -2,94 +2,99 @@
 #include <string.h>
 #include <stdlib.h>
 
-void swap(char a[10],char b[10]){
-  char temp[10];
-  strcpy(temp,b);
-  strcpy(b,a);
-  strcpy(a,temp);
+void swap(char a[10], char b[10])
+{
+    char temp[10];
+    strcpy(temp, b);
+    strcpy(b, a);
+    strcpy(a, temp);
 }
 
- void quicksort (char a[][10], long l, long r){
-     char v[10];
-     strcpy(v,a[r]);
-     if (r <= l)
+void quicksort(char Country[][10], char Name[][10], long l, long r)
+{
+    char v[10];
+    strcpy(v, Country[r]);
+    if (r <= l)
         return;
-     long i = l;
-     long j = r - 1;
-     long p = l - 1;
-     long q = r;
-     while (i <= j){ 
-        while (strcmp(a[i],v) < 0) 
-           i++;
-        while (strcmp(a[j],v) > 0) 
-           j--;
+    long i = l;
+    long j = r - 1;
+    long p = l - 1;
+    long q = r;
+    while (i <= j)
+    {
+        while (strcmp(Country[i], v) < 0)
+            i++;
+        while (strcmp(Country[j], v) > 0)
+            j--;
         if (i >= j)
-          break;
-        swap(a[i], a[j]);
-        if (strcmp(a[i],v) == 0)
-           p++;
-           swap(a[p], a[i]);
+            break;
+        swap(Country[i], Country[j]);
+        swap(Name[i], Name[j]);
+        if (strcmp(Country[i], v) == 0)
+            p++;
+        swap(Country[p], Country[i]);
+        swap(Name[p], Name[i]);
         i++;
-        if (strcmp(a[j],v) == 0);
-           q--;
-           swap(a[q], a[j]);
-        j--;}
-     swap(a[i], a[r]);
-     j = i - 1;
-     i++;
-     for (long k = l; k <= p; k++, j--) 
-        swap(a[k], a[j]);
-     for (long k = r - 1; k >= q; k--, i++) 
-        swap(a[k], a[i]); 
-     quicksort(a, l, j);
-     quicksort(a, i, r);}
+        if (strcmp(Country[j], v) == 0)
+        q--;
+        swap(Country[q], Country[j]);
+        swap(Name[q], Name[j]);
+        j--;
+    }
+    swap(Country[i], Country[r]);
+    swap(Name[i], Name[r]);
+    j = i - 1;
+    i++;
+    for (long k = l; k <= p; k++, j--)
+    {
+        swap(Country[k], Country[j]);
+        swap(Name[k], Name[j]);
+    }
+    for (long k = r - 1; k >= q; k--, i++)
+    {
+        swap(Country[k], Country[i]);
+        swap(Name[k], Name[i]);
+    }
+    quicksort(Country, Name, l, j);
+    quicksort(Country, Name, i, r);
+}
 
-     
 void main()
 {
 
-    FILE *fin, *fout;
-    fin = fopen("race.in", "r");
-    fout = fopen("race.out", "w");
-    long n;
-    long k = 0;
-    fscanf(fin, "%ld", &n);
-    char Country[n][10], Name[n][10], SortedCountries[n][10], SortedCountriesOne[200][10];
-    
-    //Ввод
-    for (int i = 0; i < n; i++)
+  FILE *fin, *fout;
+  fin = fopen("race.in", "r");
+  fout = fopen("race.out", "w");
+  long n;
+  fscanf(fin, "%ld", &n);
+  char Country[n][10], Name[n][10];
+
+  //Ввод
+  for (int i = 0; i < n; i++)
+  {
+    fscanf(fin, "%s", Country[i]);
+    fscanf(fin, "%s", Name[i]);
+  }
+
+  //Сортировка по алфавиту
+  quicksort(Country, Name, 0, n);
+
+  //вывод
+  for (int i = 0; i < n; i++)
+  {
+    if (i == 0)
     {
-        fscanf(fin, "%s", Country[i]);
-        fscanf(fin, "%s", Name[i]);
+      fprintf(fout, "=== %s ===\n", Country[i]);
+      fprintf(fout, "%s\n", Name[i]);
     }
-
-    //Копируем массив, который будем сортировать
-    memcpy (SortedCountries, Country, sizeof(Country)); 
-
-    //Сортировка по алфавиту
-    quicksort (SortedCountries, 0, n);
-    
-    for(int i=0; i<n;i++) printf("%s", SortedCountries[i]);
-
-    //Копируем первый элемент для сравнения 
-    strcpy(SortedCountriesOne[0], SortedCountries[0]);
-
-    //Создаем массив без повторяющихся элементов
-    for (int i = 1; i <= n; i++)
-        if (strcmp(SortedCountries[i], SortedCountries[i - 1]) != 0)
-        {
-            ++k;
-            strcpy(SortedCountriesOne[k], SortedCountries[i]);
-        }
-
-    //вывод
-    for(long i = 0; i < k; i++){
-        fprintf(fout, "=== %s ===\n", SortedCountriesOne[i]);
-        for(long j = 0; j < n; j++){
-            if(strcmp(SortedCountriesOne[i], Country[j]) == 0){
-                fprintf(fout, "%s\n", Name[j]);
-            }
-        }
-
+    else if (strcmp(Country[i], Country[i - 1]) == 0)
+    {
+      fprintf(fout, "%s\n", Name[i]);
     }
+    else
+    {
+      fprintf(fout, "=== %s ===\n", Country[i]);
+      fprintf(fout, "%s\n", Name[i]);
+    }
+  }
 }
